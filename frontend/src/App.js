6e8fcb66275e25d24821c83a88b1a29e3d549e3d
Nic Marchini <nicmarchini@gmail.com>
2022-01-14 20:55:55 -0500

    additional card assets, small cleanup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,29 +1,15 @@
-// import logo from './logo.svg';
 import './css/normalize.css';
 import './css/skeleton.css';
 import './css/style.css';
 import React from 'react';
 import axios from 'axios';
-// import Card from './components/card';
-import Draggable from 'react-draggable';
 import './App.css'
-// import club from './assets/basic-lorg-card-club.png';
-import heart from './assets/basic-lorg-card-heart.png';
 import spade from './assets/basic-lorg-card-spade.png';
-import diamond from './assets/basic-lorg-card-diamond.png';
 import back from './assets/basic-lorg-card-back4Bs.png';
 import './components/card.css';
-import { clubs } from './components/images'
-
+import { clubs, spades, hearts, diamonds } from './components/images'
 
 class App extends React.Component{
-
-
-  // {/* <img src={images['0.png']} /> */}
-  
-  
-
-
   onDragOver = (ev) => {
     ev.preventDefault();
   }
@@ -93,7 +79,6 @@ class App extends React.Component{
         //   a: 10,
         //   b: 20
         // }
-
       };
     
     axios(options)
@@ -101,7 +86,6 @@ class App extends React.Component{
         console.log(response);
       });
       this.getAllCards()
-      
     }
 
     setCards(stack, cardlist){
@@ -112,7 +96,6 @@ class App extends React.Component{
       results = [].concat(cardlist);
       localStorage.setItem('s'+stack, JSON.stringify(results));
       this.getCards(stack);
-
     }
 
     clearCards(stack){
@@ -143,9 +126,10 @@ class App extends React.Component{
         if (cardnum == "J"){cardnum = 11}
         if (cardnum == "A"){cardnum = 1}
         console.log("results look like:",i, results[i])
-        if (results[i][1] == 'Hearts'){foto = heart;}
+        if (results[i][1] == 'Spades'){foto = spades[cardnum-1];}
+        if (results[i][1] == 'Hearts'){foto = hearts[cardnum-1];}
         if (results[i][1] == 'Clubs'){foto = clubs[cardnum-1] }
-        if (results[i][1] == 'Diamonds'){foto = diamond;}
+        if (results[i][1] == 'Diamonds'){foto = diamonds[cardnum-1];}
         if (results[i] == 'flipped'){foto = back;}
         newtasks.push({name:Math.random(), category:stack_num, bgcolor:"skyblue", index:"0", foto:<img src={foto}  draggable='true' className="photo-s" alt={cardnum} />})
       }
@@ -153,13 +137,9 @@ class App extends React.Component{
       this.setState({ ['tasks'] : newtasks})
 
     }
-  
-
-    
 
     getAllCards(){
         // let data = []
-
         const options = {
           url: 'http://localhost:5000/getcards',
           method: 'GET',
@@ -179,9 +159,6 @@ class App extends React.Component{
         .then(response => {
           // console.log(JSON.parse(response.data));
           let data = JSON.parse(response.data)
-
-
-
           for (var i = 0; i < 10; i++) {
             this.setCards(i, data[i])
             this.getCards(i)
@@ -191,7 +168,6 @@ class App extends React.Component{
     }
 
     deal(){
-
       const options = {
         url: 'http://localhost:5000/deal',
         method: 'POST',
@@ -204,12 +180,8 @@ class App extends React.Component{
       });
       this.getAllCards()
   }
-
   
-    render() {  
-
-
-
+    render() {
       var columns = [];
       var tasks = {};    
       for(var i = 0; i < 10; i++){
@@ -217,79 +189,33 @@ class App extends React.Component{
           tasks[name]=[];
 
           columns.push(
-                  <div className="wip one column"
-                  style={{paddingTop:92}}
-                  onDragOver={(e)=>this.onDragOver(e)}
-                  onDrop={(e)=>{this.onDrop(e, name)}}>
-                    
-                  {/* <span className="task-header">COL1</span> */}
-                  {tasks[name]}
-                   </div> 
+                 <div className="wip one column"
+                      style={{paddingTop:92}}
+                      onDragOver={(e)=>this.onDragOver(e)}
+                      onDrop={(e)=>{this.onDrop(e, name)}}>
+                      {tasks[name]}
+                  </div> 
           );
-
           console.log(tasks)
       }
 
-
-
       this.state.tasks.forEach ((t) => {                   
-          tasks[t.category].push(<div key={t.name}                           
-                                      onDragStart={(e)=>this.onDragStart(e, t.name)}
-                                      draggable
-                                      className="draggable"
-                                      style={ {marginTop:-92}
-                                        // {backgroundColor: t.bgcolor}
-                                      }
-                                      >                                
-                                      {t.foto}
-              </div>
-              );
-          });
-  
-
-
-
-
-
-      
-        var rows = [];
-        for (var i = 0; i < 10; i++) {
-            let num = i
-            rows.push(
-              
-              //math.random not ideal solution here migth cause extra re-rendering due to key not being stable can fix later
-              <div key={Math.random()} className="one column">
-                {/* stack {i} */}
-
-                      
-                      {/* <button onClick={() => this.setCards(num, [1,2,3,4,5]) }>Add</button> */}
-                      {/* <button onClick={() => this.clearCards(num) }>Clear</button> */}
-                      {/* { new Card('spade', '4').render() */}
-                      {/* } */}
-
-                      {/* {this.state['s'+num].map(function(card,index){
-                          if (card !== null){
-                            return(
-                              //see above about key
-                              
-                              <p key={Math.random()}>{card[0] + " of " + card[1]}</p>
-                            )
-                          } else {
-                            return (
-                              <p></p>
-                             )
-                          }
-
-                      })} */}
-
-                    </div>);
-
-        }
-      
+          tasks[t.category].push(
+             <div key={t.name}                           
+                  onDragStart={(e)=>this.onDragStart(e, t.name)}
+                  draggable
+                  className="draggable"
+                  style={ {marginTop:-92}
+                    // {backgroundColor: t.bgcolor}
+                  }
+                >                                
+                {t.foto}
+            </div>
+          );
+      });
 
     return (
         <div id='parent'>
-            
             <div  className="container">
               <h2 style={{color:'rgb(255, 245, 238)'}}>nics-spider-solitaire</h2>
                 <button style={{color:'rgb(255, 245, 238)'}} onClick={() => this.newGame()}>New Game</button>
@@ -297,13 +223,9 @@ class App extends React.Component{
                 <button style={{color:'rgb(255, 245, 238)'}} onClick={() => this.deal()}>Deal</button>
               <div className="example-grid docs-example">
               <div style={{paddingTop:10}}></div>
-
               <div className="row">
                   <div className="one column"></div>
-
                   {columns}
-
-
                   <div className="one column"></div>
                 </div>
             </div>
--- frontend/src/components/images.js
+++ frontend/src/components/images.js
@@ -12,6 +12,48 @@ import club11 from '../assets/clubs/j-club.png';
 import club12 from '../assets/clubs/q-club.png';
 import club13 from '../assets/clubs/k-club.png';
 
+import spade1 from  '../assets/spades/a-s.png';
+import spade2 from  '../assets/spades/2-s.png';
+import spade3 from  '../assets/spades/3-s.png';
+import spade4 from  '../assets/spades/4-s.png';
+import spade5 from  '../assets/spades/5-s.png';
+import spade6 from  '../assets/spades/6-s.png';
+import spade7 from  '../assets/spades/7-s.png';
+import spade8 from  '../assets/spades/8-s.png';
+import spade9 from  '../assets/spades/9-s.png';
+import spade10 from '../assets/spades/10-s.png';
+import spade11 from '../assets/spades/j-s.png';
+import spade12 from '../assets/spades/q-s.png';
+import spade13 from '../assets/spades/k-s.png';
+
+import diamond1 from  '../assets/diamonds/1-d.png';
+import diamond2 from  '../assets/diamonds/2-d.png';
+import diamond3 from  '../assets/diamonds/3-d.png';
+import diamond4 from  '../assets/diamonds/4-d.png';
+import diamond5 from  '../assets/diamonds/5-d.png';
+import diamond6 from  '../assets/diamonds/6-d.png';
+import diamond7 from  '../assets/diamonds/7-d.png';
+import diamond8 from  '../assets/diamonds/8-d.png';
+import diamond9 from  '../assets/diamonds/9-d.png';
+import diamond10 from '../assets/diamonds/10-d.png';
+import diamond11 from '../assets/diamonds/j-d.png';
+import diamond12 from '../assets/diamonds/q-d.png';
+import diamond13 from '../assets/diamonds/k-d.png';
+
+import heart1 from  '../assets/hearts/a-h.png';
+import heart2 from  '../assets/hearts/2-h.png';
+import heart3 from  '../assets/hearts/3-h.png';
+import heart4 from  '../assets/hearts/4-h.png';
+import heart5 from  '../assets/hearts/5-h.png';
+import heart6 from  '../assets/hearts/6-h.png';
+import heart7 from  '../assets/hearts/7-h.png';
+import heart8 from  '../assets/hearts/8-h.png';
+import heart9 from  '../assets/hearts/9-h.png';
+import heart10 from '../assets/hearts/10-h.png';
+import heart11 from '../assets/hearts/j-h.png';
+import heart12 from '../assets/hearts/q-h.png';
+import heart13 from '../assets/hearts/k-h.png';
+
 export const clubs = [
     club1,
     club2,
@@ -26,4 +68,52 @@ export const clubs = [
     club11,
     club12,
     club13
+];
+
+export const hearts = [
+    heart1,
+    heart2,
+    heart3,
+    heart4,
+    heart5,
+    heart6,
+    heart7,
+    heart8,
+    heart9,
+    heart10,
+    heart11,
+    heart12,
+    heart13
+];
+
+export const diamonds = [
+    diamond1,
+    diamond2,
+    diamond3,
+    diamond4,
+    diamond5,
+    diamond6,
+    diamond7,
+    diamond8,
+    diamond9,
+    diamond10,
+    diamond11,
+    diamond12,
+    diamond13
+];
+
+export const spades = [
+    spade1,
+    spade2,
+    spade3,
+    spade4,
+    spade5,
+    spade6,
+    spade7,
+    spade8,
+    spade9,
+    spade10,
+    spade11,
+    spade12,
+    spade13
 ];
